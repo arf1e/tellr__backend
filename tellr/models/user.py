9be73540849082dc8dd64d0146a9a70784e4c7d6
arf1e <anotheruselessbox@gmail.com>
2018-12-13 03:06:11 +0300

    :revolving_hearts: Допилен логаут, добавлен эндпоинт поиска юзеров :revolving_hearts:

--- tellr/models/user.py
+++ tellr/models/user.py
@@ -1,61 +1,64 @@
 from tellr.db import db
+
 Model = db.Model
 from datetime import date
 from tellr.utils import calculate_age
+from sqlalchemy.sql.expression import func, select
+
 
 class UserModel(Model):
-  __tablename__ = 'users'
+    __tablename__ = "users"
 
-  # Поля
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(30), unique=True, nullable=False)
-  password = db.Column(db.String)
-  # Определять пол булеаном меня научили еще в институте, 
-  # так что нихельпихель может написать своё приложение с гендерами и прочей хуетой
-  sex = db.Column(db.Boolean)
-  first_name = db.Column(db.String)
-  birthday = db.Column(db.DateTime, default=date(1997, 10, 16))
-  
-  def __init__(self, username, password, first_name, sex):
-    self.username = username
-    self.password = password
-    self.first_name = first_name
-    self.sex = sex
+    # Поля
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    # Определять пол булеаном меня научили еще в институте,
+    # так что нихельпихель может написать своё приложение с гендерами и прочей хуетой
+    sex = db.Column(db.Boolean)
+    first_name = db.Column(db.String)
+    birthday = db.Column(db.DateTime, default=date(1997, 10, 16))
 
-  # Служебные методы
-  def json(self):
-    return {
-      'id': self.id,
-      'username': self.username,
-      'first_name': self.first_name,
-      'age': calculate_age(self.birthday),
-      'sex': 'Male' if self.sex else 'Female'
-    }
+    def __init__(self, username, password, first_name, sex):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.sex = sex
 
-  # Взаимодействие с БД
-  def save_to_db(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def delete_from_db(self):
-    db.session.delete(self)
-    db.session.commit()
+    # Служебные методы
+    def json(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "age": calculate_age(self.birthday),
+            "sex": "Male" if self.sex else "Female",
+        }
 
-  # Ещё кое-какие нужные служебные методы, но это уже для дальнейших манипуляций
-  @classmethod
-  def find_by_username(cls, username):
-    return cls.query.filter_by(username=username).first()
-  
-  @classmethod
-  def find_by_id(cls, _id):
-    return cls.query.filter_by(id=_id).first()
-  
-  @classmethod
-  def find_males(cls):
-    return cls.query.filter_by(sex=True)
-  
-  @classmethod
-  def find_females(cls):
-    return cls.query.filter_by(sex=False)
+    # Взаимодействие с БД
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-  
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # Ещё кое-какие нужные служебные методы, но это уже для дальнейших манипуляций
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def find_males(cls):
+        return cls.query.filter_by(sex=True).order_by(func.random()).limit(4)
+
+    @classmethod
+    def find_females(cls):
+        return cls.query.filter_by(sex=False).order_by(func.random()).limit(4)
+
+    
--- tellr/resources/user.py
+++ tellr/resources/user.py
@@ -1,52 +1,96 @@
 from flask_restplus import Resource, reqparse
 from tellr.models.user import UserModel
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_raw_jwt,
+    get_jwt_identity,
+)
+from tellr.blacklist import BLACKLIST
 
 
 # TODO: Сделать разные парсеры для флоу регистрации и остальных, я сейчас слишком хочу спать даже для этого
-_user_parser = reqparse.RequestParser()
-_user_parser.add_argument('username', type=str, required=True, help='no username')
-_user_parser.add_argument('password', type=str, required=True, help='no password')
-_user_parser.add_argument('first_name', type=str)
-_user_parser.add_argument('sex', type=bool)
+user_parser = reqparse.RequestParser()
+user_parser.add_argument("username", type=str, required=True, help="no username")
+user_parser.add_argument("password", type=str, required=True, help="no password")
+user_parser.add_argument("first_name", type=str)
+user_parser.add_argument("sex", type=bool)
+
 
 class UserRegister(Resource):
-  def post(self):
-    data = _user_parser.parse_args()
-    if UserModel.find_by_username(data['username']):
-      return {'msg': 'user exists'}, 400
-    user = UserModel(**data)
-    user.save_to_db()
-    return {'msg': 'user created'}, 201
+    def post(self):
+
+        data = user_parser.parse_args()
+        if UserModel.find_by_username(data["username"]):
+            return {"msg": "user exists"}, 400
+        user = UserModel(**data)
+        user.save_to_db()
+        return {"msg": "user created"}, 201
+
 
 class UserLogin(Resource):
-  @classmethod
-  def post(cls):
-    data = _user_parser.parse_args()
-    user = UserModel.find_by_username(data['username'])
-    if user and safe_str_cmp(user.password, data['password']):
-      access_token = create_access_token(identity=user.id, fresh=True)
-      refresh_token = create_refresh_token(user.id)
-      return {
-        'access_token': access_token,
-        'refresh_token': refresh_token
-      }, 200
-    return {'msg': 'invalid credentials'}, 400 # unauthorized
+    @classmethod
+    def post(cls):
+        data = user_parser.parse_args()
+        user = UserModel.find_by_username(data["username"])
+        if user and safe_str_cmp(user.password, data["password"]):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return (
+                {
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": user.json()
+                },
+                200,
+            )
+        return {"msg": "invalid credentials"}, 400  # unauthorized
+
 
 class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"msg": "user not found"}, 404
+
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"msg": "user not found"}, 404
+        user.delete_from_db()
+        return {"msg": "user has been deleted"}, 200
+
+
+class UserLogout(Resource):
+    @classmethod
+    @jwt_required
+    def post(cls):
+        print(get_raw_jwt()['jti'])
+        jti = get_raw_jwt()["jti"]  # jti это "JWT ID", уникальный идентификатор JWT.
+        # По нему можно однозначно идентифицировать пользователя, что позволяет реализовать систему логаутов.
+        # Мы просто берём и добавляем этот идентификатор в блэклист, то есть тупо баним токен пользователя.
+        user_id = get_jwt_identity()
+        BLACKLIST.add(jti)
+        return {"message": f'User <id={user_id}> has logged out'}, 200
+
+
+# Поиск
+class UserQuery(Resource):
   @classmethod
-  def get(cls, user_id):
-    user = UserModel.find_by_id(user_id)
-    if not user:
-      return {'msg': 'user not found'}, 404
-    
-    return user.json()
-  
-  @classmethod
-  def delete(cls, user_id):
-    user = UserModel.find_by_id(user_id)
-    if not user:
-      return {'msg': 'user not found'}, 404
-    user.delete_from_db()
-    return {'msg': 'user has been deleted'}, 200
+  @jwt_required
+  def get(cls):
+    user_id = get_jwt_identity()
+    user = UserModel.find_by_id(user_id).json()
+    if user['sex'] == 'Male':
+      users = [user.json() for user in UserModel.find_females()]
+      return {'users': users}, 200
+    else:
+      users = [user.json() for user in UserModel.find_males()]
+      return {'users': users}, 200
+
