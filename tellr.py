bb747787e58f26adc5db58f0654005c74edaabdd
arf1e <anotheruselessbox@gmail.com>
2018-12-25 11:02:55 +0300

    мне нужно скоммитить базу для деплоя, простите

--- tellr.py
+++ tellr.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     from tellr.db import db
 
     db.init_app(app)
-    app.run(host="0.0.0.0", port=1337)
+    app.run(host="0.0.0.0", port=80)
     # 0.0.0.0 makes the app accessible from the local network
